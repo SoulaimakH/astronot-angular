8ec1c5ff3b1774c66f84263d3cb3e245d5ccd8be
Soulaima Kahla <73546882+SoulaimakH@users.noreply.github.com>
2023-11-29 21:55:27 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "image" {
 variable "port" {
   type        = number
   description = "Port to open on the container and the public IP address."
-  default     = 4200
+  default     = 80
 }
 
 variable "cpu_cores" {
